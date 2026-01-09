20f04298ba5a58a63a3e3419e04011c8f3eaec81
Octaleads <octaleadsprivatelimited@gmail.com>
2026-01-09 16:28:57 +0530

    Add LinkedIn social media link to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Youtube, Facebook, Instagram } from "lucide-react";
+import { Youtube, Facebook, Instagram, Linkedin } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const footerLinks = {
@@ -44,6 +44,7 @@ const socialLinks = [
   { icon: Youtube, href: "https://www.youtube.com/@SangronyxTechnologies", label: "YouTube" },
   { icon: Instagram, href: "https://www.instagram.com/sangronyxtech?igsh=aXVnbDVmOG9nejJj", label: "Instagram" },
   { icon: Facebook, href: "https://www.facebook.com/share/1C8oFpvEpG/", label: "Facebook" },
+  { icon: Linkedin, href: "https://www.linkedin.com/company/sangronyx/", label: "LinkedIn" },
 ];
 
 const legalLinks = [
